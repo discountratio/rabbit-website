f75248b64169891e4544ec213c1def91816f76a3
discountratio <discountratio@gmail.com>
2023-01-29 16:46:58 +0700

    added cart functionality

--- scripts/mason-gallery.js
+++ scripts/mason-gallery.js
@@ -4,62 +4,145 @@ console.log(galleryItemArray);
 const masonGallery = document.getElementById("mason-gallery");
 const cart = document.getElementById("cart");
 
-function addToCart(name, price, size, image) {
-  const cartItem = document.createElement("div");
-  const cartItemName = document.createElement("h3");
-  const cartItemPrice = document.createElement("p");
-  const cartItemImage = document.createElement("img");
-  const cartItemSize = document.createElement("p");
-  const cartItemQuantity = document.createElement("p");
-  const cartItemQuantityUp = document.createElement("button");
-  const cartItemQuantityDown = document.createElement("button");
-  const cartItemRemove = document.createElement("button");
+function addToCart(title, srcset) {
+  // Containers ------------------------------------------------------------------------------------
+  
 
-  cartItemName.textContent = name;
-  cartItemPrice.textContent = price;
-  cartItemImage.src = image;
-  cartItemSize.textContent = size;
-  cartItemQuantity.textContent = 1;
+
+
+  // elements
+  const cartItem = document.createElement("div");
+  const cartItemQuantityButtons = document.createElement("div");
+  const cartItemImageContainer = document.createElement("div");
+
+  // classes
+  cartItem.classList.add("cart-item");
+  cartItemQuantityButtons.classList.add("cart-item-quantity-buttons");
+  cartItemImageContainer.classList.add("cart-item-image-container");
+
+  //components  ------------------------------------------------------------------------------------
+  const cartItemTitle = document.createElement("h3");
+  const cadrtItemImage = document.createElement("img");
+
+  // Print Sizes ------------------------------------------------------------------------------------
+  const printSizeOptions = [
+    { value: "4x6", text: "8x10", price: 4.99 },
+    { value: "5x7", text: "5x7", price: 4.99 },
+    { value: "8x10", text: "8x10", price: 7.99 },
+    { value: "11x14", text: "11x14", price: 9.99 },
+    { value: "16x20", text: "16x20", price: 14.99 },
+    { value: "20x24", text: "20x24", price: 19.99 },
+    { value: "24x36", text: "24x36", price: 24.99 },
+    { value: "30x40", text: "30x40", price: 29.99 },
+    { value: "40x60", text: "40x60", price: 39.99 },
+  ];
+
+  const printSizeSelector = document.createElement("select");
+  printSizeSelector.classList.add("print-size-selector");
+  printSizeSelector.onchange = (e) => {
+    console.log(e.target.value);
+    console.log(e.target.options[e.target.selectedIndex].price);
+    price = e.target.options[e.target.selectedIndex].price;
+    cartItemTotal.textContent = `Total: $${(price * quantity).toFixed(2)}`;
+
+  };
+
+  printSizeOptions.forEach((option) => {
+    const printSizeOption = document.createElement("option");
+    printSizeOption.value = option.value;
+    printSizeOption.price = option.price;
+    printSizeOption.text = `${option.text} - $${option.price}`;
+    printSizeSelector.add(printSizeOption);
+  });
+
+  // ----------------------------------------------------------------------------------------------
+  // Quantity ------------------------------------------------------------------------------------
+
+      //quantity variables
+      let quantity = 1;
+      // let price = 4.99;
+      let price = printSizeSelector.options[printSizeSelector.selectedIndex].price;
+
+  //displays
+  const cartItemTotal = document.createElement("p");
+  cartItemTotal.classList.add("cart-item-total");
+  cartItemTotal.textContent = `Total: $${price}`;
+
+  const cartItemQuantity = document.createElement("p");
+  cartItemQuantity.classList.add("cart-item-quantity");
+  cartItemQuantity.textContent = `Amount: ${quantity}`;
+
+  //buttons
+  const cartItemQuantityUp = document.createElement("button");
+  cartItemQuantityUp.classList.add("cart-item-quantity-up");
   cartItemQuantityUp.textContent = "+";
+
+  const cartItemQuantityDown = document.createElement("button");
+  cartItemQuantityDown.classList.add("cart-item-quantity-down");
   cartItemQuantityDown.textContent = "-";
+
+  const cartItemRemove = document.createElement("button");
+  cartItemRemove.classList.add("cart-item-remove");
   cartItemRemove.textContent = "Remove";
 
-  cartItem.classList.add("cart-item");
-  cartItemName.classList.add("cart-item-name");
-  cartItemPrice.classList.add("cart-item-price");
-  cartItemImage.classList.add("cart-item-image");
-  cartItemSize.classList.add("cart-item-size");
-  cartItemQuantity.classList.add("cart-item-quantity");
+  //event listeners
+  cartItemQuantityUp.addEventListener("click", () => {
+    quantity++;
+    cartItemQuantity.textContent = `Amount: ${quantity}`;
+    cartItemTotal.textContent = `Total: $${(price * quantity).toFixed(2)}`;
+    console.log(`quantity up: ${quantity} @cartItemQuantityUp`);
+  });
+
+  cartItemQuantityDown.addEventListener("click", () => {
+    if (quantity > 1) {
+      quantity--;
+      cartItemQuantity.textContent = `Amount: ${quantity}`;
+      cartItemTotal.textContent = `Total: $${(price * quantity).toFixed(2)}`;
+      console.log(`quantity down: ${quantity} @cartItemQuantityDown`);
+    } else {
+      console.log(`quantity zero @cartItemQuantityDown`);
+    }
+  });
+
+
+
+  cartItemRemove.addEventListener("click", function () {
+    console.log("remove @cartItemRemove");
+    cartItem.remove();
+  });
+
+  // classes
   cartItemQuantityUp.classList.add("cart-item-quantity-up");
   cartItemQuantityDown.classList.add("cart-item-quantity-down");
   cartItemRemove.classList.add("cart-item-remove");
 
-  cartItemQuantityUp.addEventListener("click", () => {
-    cartItemQuantity.textContent = parseInt(cartItemQuantity.textContent) + 1;
-  });
+  //attach
+  cartItemQuantityButtons.append(
+    cartItemQuantityUp,
+    cartItemQuantityDown,
+    cartItemRemove,
+    cartItemQuantity,
+    cartItemTotal
+  );
+  // ----------------------------------------------------------------------------------------------
+  // Image ------------------------------------------------------------------------------------
+  cartItemTitle.classList.add("cart-item-title");
+  cadrtItemImage.classList.add("cart-item-image");
 
-  cartItemQuantityDown.addEventListener("click", () => {
-    if (parseInt(cartItemQuantity.textContent) > 1) {
-      cartItemQuantity.textContent = parseInt(cartItemQuantity.textContent) - 1;
-    }
-  });
+  cartItemTitle.textContent = title;
+  cadrtItemImage.srcset = srcset;
+  cadrtItemImage.src = srcset.split(",")[0].trim();
+  cadrtItemImage.alt = title;
 
-  cartItemRemove.addEventListener("click", () => {
-    cartItem.remove();
-  });
+  cartItemImageContainer.append(cadrtItemImage);
+  // ----------------------------------------------------------------------------------------------
 
-  cartItem.appendChild(cartItemName);
-  cartItem.appendChild(cartItemPrice);
-  cartItem.appendChild(cartItemImage);
-  cartItem.appendChild(cartItemSize);
-
-  const cardQuantityButtonDiv = document.createElement("div");
-  cardQuantityButtonDiv.classList.add("cart-item-quantity-buttons");
-  cardQuantityButtonDiv.appendChild(cartItemQuantityDown);
-  cardQuantityButtonDiv.appendChild(cartItemQuantity);
-  cardQuantityButtonDiv.appendChild(cartItemQuantityUp);
-
-  cartItem.appendChild(cardQuantityButtonDiv);
+  cartItem.append(
+    cartItemTitle,
+    cartItemImageContainer,
+    printSizeSelector,
+    cartItemQuantityButtons
+  );
 
   cart.appendChild(cartItem);
 }
@@ -70,10 +153,12 @@ function makeMasonItem(type, title, subtitle, date, hqSrc, lqSrc, description) {
   const masonItem = document.createElement("div"); //container
   const masonItemImageContainer = document.createElement("div"); //image container
   const masonItemButtonContainer = document.createElement("div"); //button container
+  const masonItemTextContainer = document.createElement("div"); //text container
   // classes
   masonItem.classList.add("mason-item");
   masonItemImageContainer.classList.add("mason-item-image-container");
   masonItemButtonContainer.classList.add("mason-item-button-container");
+  masonItemTextContainer.classList.add("mason-item-text-container");
 
   // ----------------------------------------------------------------------------------------------
 
@@ -98,8 +183,9 @@ function makeMasonItem(type, title, subtitle, date, hqSrc, lqSrc, description) {
   masonItemSubtitle.textContent = subtitle;
   masonItemDate.textContent = date;
   masonItemDescription.textContent = description;
+
   //attach
-  masonItem.append(
+  masonItemTextContainer.append(
     masonItemType,
     masonItemTitle,
     masonItemSubtitle,
@@ -107,6 +193,7 @@ function makeMasonItem(type, title, subtitle, date, hqSrc, lqSrc, description) {
     masonItemImageContainer,
     masonItemDescription
   );
+  masonItem.append(masonItemTextContainer);
   //  ----------------------------------------------------------------------------------------------
 
   //  Mason item image ------------------------------------------------------------------------------
@@ -122,6 +209,7 @@ function makeMasonItem(type, title, subtitle, date, hqSrc, lqSrc, description) {
 
   //attach
   masonItemImageContainer.append(masonItemImage);
+  masonItem.append(masonItemImageContainer);
   //  ----------------------------------------------------------------------------------------------
 
   //  Mason item buttons ----------------------------------------------------------------------------
@@ -138,36 +226,40 @@ function makeMasonItem(type, title, subtitle, date, hqSrc, lqSrc, description) {
 
   //content
   masonItemAddToCardButton.textContent = "Add to Cart";
-  masonItemCloseButton.textContent = "Close";
+  masonItemCloseButton.textContent = "X";
 
   //attach
-  masonItemButtonContainer.append(
-    masonItemAddToCardButton,
-    masonItemCloseButton
-  );
+  masonItem.append(masonItemAddToCardButton, masonItemCloseButton);
 
   //event listenrs
-  masonItemAddToCardButton.addEventListener("click", () => {
-     addToCart(title, price, size, image);
+  masonItemAddToCardButton.addEventListener("click", (e) => {
+    // e.stopPropagation();
+
+    const title = masonItemTitle.textContent;
+    const srcset = masonItemImage.srcset;
+    addToCart(title, srcset);
+
+    removeActiveClass();
+    console.log("add to cart");
   });
 
-  masonItemCloseButton.addEventListener("click", () => {
+  masonItemCloseButton.addEventListener("click", (e) => {
+    e.stopPropagation(); //prevents click on parent
     removeActiveClass();
+    console.log("close");
   });
 
   // ----------------------------------------------------------------------------------------------
 
   // More stuff
   //Makes item active on click
-  masonItem.addEventListener("click", () => {
+  masonItem.addEventListener("click", (e) => {
+    e.stopPropagation();
     addActiveClass();
   });
 
   const removeActiveClass = () => {
-    const active = document.querySelector(".mason-item--active");
-    if (active) {
-      active.classList.remove("mason-item--active");
-    }
+    masonItem.classList.remove("mason-item--active");
   };
 
   //Selects all mason items and removes active class from them, then attaches active class to clicked item
@@ -177,6 +269,7 @@ function makeMasonItem(type, title, subtitle, date, hqSrc, lqSrc, description) {
       item.classList.remove("mason-item--active");
     });
     masonItem.classList.add("mason-item--active");
+    console.log("open");
   };
 
   return masonItem;
